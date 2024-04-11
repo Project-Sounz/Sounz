3abf02a781bd4c44b813020cc965b48a58161f2d
Abin Joshy <abinjkakkanattu.97@gmail.com>
2024-04-11 21:50:13 +0530

    models created

--- Sounz/sounzproject/post/models.py
+++ Sounz/sounzproject/post/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class PostDB(models.Model):
+    pid=models.IntegerField(max_length=10,primary_key = True)
+    uid=models.ForeignKey('UserDB',on_delete=models.CASCADE)
+    media_address=models.CharField(max_length=256)
+    likescount=models.IntegerField(default=0)
+    tags=models.CharField(max_length=32,null=True)
+    mediatype=models.CharField(max_length=20)
+    title = models.CharField(max_length=200)
+    descr=models.TextField()
+    lang=models.CharField(max_length=10)
+    timestamp=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self)->int:
+        return self.pid
+
+
--- Sounz/sounzproject/users/models.py
+++ Sounz/sounzproject/users/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class UserDB(models.Model):
+    uid=models.IntegerField(max_length=10,primary_key=True)
+    username=models.CharField(max_length=25)
+    password = models.CharField(max_length=15)
+    firstname=models.CharField(max_length=30)
+    lastname=models.CharField(max_length=30)
+    email = models.EmailField()
+    prof_picaddress=models.CharField(max_length=100)
+    user_bio=models.TextField()
+    phone=models.IntegerField()
+    specialised=models.CharField(max_length=40)
+    timestamp=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> int:
+        return self.uid
+    
+class SavedDB(models.Model):
+    uid=models.ForeignKey('UserDB',on_delete=models.CASCADE)
+    pid=models.ForeignKey('PostDB',on_delete=models.CASCADE)    
+    
+class CollabDB(models.Model):
+    col_id=models.IntegerField(max_length=10,primary_key=True)
+    uid=models.ForeignKey('UserDB',on_delete=models.CASCADE)
+    pid=models.ForeignKey('PostDB',on_delete=models.CASCADE)     
+
+
