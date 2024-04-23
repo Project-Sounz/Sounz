5cdf2e3ba2e27157402cf943f8af8c18a83ce548
Abin Joshy <abinjkakkanattu.97@gmail.com>
2024-04-23 20:52:00 +0530

    Reg form 2 added

--- Sounz/sounzproject/users/views.py
+++ Sounz/sounzproject/users/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def log(request):
     return render(request,'accounttest.html')
+
+def reg2(request):
+    return render(request,"reg2.html")
