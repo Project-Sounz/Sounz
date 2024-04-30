811ed100bcb552bb1eabb8cb17160e0a2980c27b
AsishChandra <asishchandra82@gmail.com>
2024-04-30 22:47:25 +0530

    navbar-profile-check

--- Sounz/sounzproject/users/urls.py
+++ Sounz/sounzproject/users/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('my-profile', views.profile_fpv, name = 'my-profile'),
     path('watch-profile', views.profile_tpv, name = 'watch-profile'),
     path('upload', views.upload, name = 'upload'),
+    path('fpv_profile', views.profile_fpv, name='fpv-profile'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
