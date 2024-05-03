fa8e0793e6a08719034c5818254b9530dc102c8d
Abin Joshy <abinjkakkanattu.97@gmail.com>
2024-05-03 11:18:27 +0530

    unkn

--- Sounz/sounzproject/users/urls.py
+++ Sounz/sounzproject/users/urls.py
@@ -15,13 +15,7 @@ urlpatterns = [
     path('watch-profile', views.profile_tpv, name = 'watch-profile'),
     path('upload', views.upload, name = 'upload'),
     path('profile/<str:username>/', views.profile, name='profile'),
-<<<<<<< HEAD
-    path('fpv_profile', views.profile_fpv, name='fpv-profile'),
-    path('editprofile',views.editprofile,name='editprofile')
-    
-=======
     path('editprofile',views.editprofile, name='editprofile'),
     path('my-profile-saved', views.nav_saved, name='nav-saved'),
->>>>>>> 11d1af0daf39ac8e67b635e6b484d6aed413bc80
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
