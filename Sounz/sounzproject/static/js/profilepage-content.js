e1e714293d6ec4603fdbe025d22b09e3d8b36118
rejath <rejath2003@gmail.com>
2024-05-07 11:21:39 +0530

    unknown

--- Sounz/sounzproject/static/js/profilepage-content.js
+++ Sounz/sounzproject/static/js/profilepage-content.js
@@ -1,10 +1,16 @@
-function displayposts(){
+// function displayposts(){
+//     document.getElementById('profile-content-controller-posts').style.display = 'block';
+//     document.getElementById('profile-content-controller-saved').style.display = 'none';
+//     document.getElementById('profile-content-controller-nomedia').style.display = 'none';
+// }
+// function displaysaved(){
+//     document.getElementById('profile-content-controller-posts').style.display = 'none';
+//     document.getElementById('profile-content-controller-nomedia').style.display = 'none';
+//     document.getElementById('profile-content-controller-saved').style.display = 'block';
+// }
+
+document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('profile-content-controller-posts').style.display = 'block';
     document.getElementById('profile-content-controller-saved').style.display = 'none';
     document.getElementById('profile-content-controller-nomedia').style.display = 'none';
-}
-function displaysaved(){
-    document.getElementById('profile-content-controller-posts').style.display = 'none';
-    document.getElementById('profile-content-controller-nomedia').style.display = 'none';
-    document.getElementById('profile-content-controller-saved').style.display = 'block';
-}
+});
