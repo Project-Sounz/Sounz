c45dafab1eb050aac428b94cd39e8110f2dd77ad
AsishChandra <asishchandra82@gmail.com>
2024-05-07 16:10:16 +0530

    somethings are back

--- Sounz/sounzproject/users/models.py
+++ Sounz/sounzproject/users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
+from django.utils import timezone
+
 
 
 # Create your models here.
@@ -31,6 +33,7 @@ class postdb(models.Model):
     mediatype=models.CharField(max_length=15)
     location=models.CharField(max_length=15)
     likes=models.IntegerField(default=0)
+    timestamp = models.DateTimeField(auto_now=True  )
     
     def __str__(self):
         return str(self.pid)
@@ -38,6 +41,7 @@ class postdb(models.Model):
 class saveddb(models.Model):
     username=models.ForeignKey(to=profiledatadb,on_delete=models.CASCADE)
     pid=models.ForeignKey(to=postdb,on_delete=models.CASCADE)
+
 class postdatabase(models.Model):
     username=models.ForeignKey(to=profiledatadb,on_delete=models.CASCADE,default=0)
     media_address = models.CharField(max_length=256)
