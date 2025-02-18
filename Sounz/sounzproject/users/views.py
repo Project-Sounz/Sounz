d315607095da6b9c5e08e6ed12b16d9f9458a12e
Benet Paul Benny <benetpaulbenny@gmail.com>
2025-02-19 02:15:29 +0530

    collab db initiated

--- Sounz/sounzproject/users/views.py
+++ Sounz/sounzproject/users/views.py
@@ -679,7 +679,22 @@ def editpost(request):
 
     return render(request, 'edit-post.html', {'user': userobj, 'post': pos, 'form': form})
 
+def save_collab(request):
+    username=request.user.username
 
+    if request.method == "POST":
+        post_id = request.POST.get("post_id")
+        post = get_object_or_404(postdb, pid=post_id)
+        base_plan = request.POST.get("base-plan")
+        
+        collab = Collab_Information.objects.create(
+            base_post_id=post,
+            base_plan=base_plan,
+            collab_requestor=username
+            )
+        print(f"Collaboration Created: {collab.collaboration_Id}")
+
+        
 #delete post
 def delete_post(request, post_id):
     if request.method == "POST":
